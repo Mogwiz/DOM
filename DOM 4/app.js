fd65182cec87e9d8e11b6234e506f70498c1df5c
Mogwiz <lbentbagdad@gmail.com>
2024-05-29 11:49:47 +0200

    DOM 6 - ex 1

---
+++ DOM 4/app.js
@@ -0,0 +1,30 @@
+const bold = document.querySelector("b");
+
+bold.parentElement.parentElement.parentElement.parentElement.style.backgroundColor = "beige";
+
+bold.style.color = "red";
+
+bold.parentElement.parentElement.style.backgroundColor = "darkblue";
+
+bold.parentElement.parentElement.parentElement.style.color = "white";
+
+bold.parentElement.nextElementSibling.style.color = "pink";
+
+
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.lastChild.previousSibling.firstChild.nextSibling.style.color = "orange";
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.lastChild.previousSibling.firstChild.nextSibling.nextElementSibling.style.color = "orange";
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.lastChild.previousSibling.firstChild.nextSibling.nextElementSibling.nextElementSibling.style.color = "orange";
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.lastChild.previousSibling.firstChild.nextSibling.nextElementSibling.nextElementSibling.nextElementSibling.style.color = "orange";
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.lastChild.previousSibling.firstChild.nextSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.style.color = "orange";
+
+
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.lastChild.previousSibling.firstChild.nextSibling.firstChild.style.color = "green";
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.lastChild.previousSibling.firstChild.nextSibling.nextElementSibling.firstChild.style.color = "green";
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.lastChild.previousSibling.firstChild.nextSibling.nextElementSibling.nextElementSibling.firstChild.style.color = "green";
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.lastChild.previousSibling.firstChild.nextSibling.nextElementSibling.nextElementSibling.nextElementSibling.firstChild.style.color = "green";
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.lastChild.previousSibling.firstChild.nextSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.firstChild.style.color = "green";
+
+bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.firstChild.nextSibling.style.textTransform = "uppercase";
+
+console.log(bold.parentElement.parentElement.parentElement.previousElementSibling.firstChild.nextSibling.firstChild.nextSibling);
+
